d5306a397ee0ae6f86fea9f05394c3eaf8f45a9b
VedantYadav025 <136351429+VedantYadav025@users.noreply.github.com>
2025-12-30 14:27:19 +0530

    Configure Emacs to disable bell and enable relative line numbers

---
+++ .emacs.d/init.el
@@ -0,0 +1,9 @@
+;; Disable the ring bell feature
+(setq ring-bell-function 'ignore)
+(setq visible-bell t)
+
+;; Set the type of line numbering to relative
+(setq display-line-numbers-type 'relative) 
+
+;; Activate the line numbers globally in all buffers
+(global-display-line-numbers-mode)
