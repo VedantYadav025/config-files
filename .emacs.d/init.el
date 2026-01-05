96783fdbb9f1bcb47501558573306b8e558c5a83
VedantYadav025 <136351429+VedantYadav025@users.noreply.github.com>
2026-01-05 16:21:53 +0530

    Enhance Emacs configuration with packages and modes
    Added package management and configuration for use-package, which-key, and Haskell mode. Enabled global line numbers and Evil mode.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,9 +1,53 @@
-;; Disable the ring bell feature
+(setq inhibit-startup-message t)
 (setq ring-bell-function 'ignore)
-(setq visible-bell t)
+(setq display-line-numbers-type 'relative)
+(global-display-line-numbers-mode 1)
 
-;; Set the type of line numbering to relative
-(setq display-line-numbers-type 'relative) 
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives
+      '(("gnu"   . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")))
+(add-to-list 'package-archives
+	     '("melpa", "https://melpa.org/packages/"))
 
-;; Activate the line numbers globally in all buffers
-(global-display-line-numbers-mode)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+	(package-refresh-contents)
+	(package-install 'use-package))
+
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+;; Evil Mode Config
+(evil-mode 1)
+(setq evil-insert-state-cursor 'box)
+
+;; Haskell Setup
+(haskell-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+(interactive-haskell-mode 1)
+(add-hook 'haskell-mode-hook #'interactive-haskell-mode)
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("e13beeb34b932f309fb2c360a04a460821ca99fe58f69e65557d6c1b10ba18c7"
+     default))
+ '(package-selected-packages '(evil gruber-darker-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
